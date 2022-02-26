cdf87c7ddd0d89c48b011ede027f57d8ab5caddc
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2022-02-25 22:11:37 -0500

    Simplify Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,12 @@ export default function NavBar() {
   return (
     <Card>
       <Card.Footer>
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-sm-3 mx-4">
-          <p className="col-md-4 mb-0 text-muted">© 2022 Cégep régional de Lanaudière à Joliette</p>
-          <a className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
-            href="/">
-            <img height={45} width={38} src={Logo} className="d-inline-block align-top"  alt="Logo"/>
-          </a>
-          <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><a className="nav-link px-2 text-muted" href="https://cegep-lanaudiere-joliette.omnivox.ca">Retour à Omnivox</a></li>
-          </ul>
+        <footer className="navbar navbar-inverse navbar-fixed-bottom">
+          <p className="h5 text-muted">© 2022 Cégep régional de Lanaudière à Joliette</p>
+          <a href="/"><img height={52} width={43} src={Logo} alt="Logo" /></a>
+          <a className="h5 nav-link text-muted" href="https://cegep-lanaudiere-joliette.omnivox.ca">Retour à Omnivox</a>
         </footer>
       </Card.Footer>
     </Card>
-    );
+  );
 }
