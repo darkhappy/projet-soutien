830be861ed6c070bd3d8d29c4038a48456d58c96
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2022-02-26 11:24:21 -0500

    Fixing colomn width

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function NavBar() {
     <Card>
       <Card.Footer>
           <div class="row">
-            <div class="col my-auto text-start"><p className="h5 text-muted">©2022 Cégep régional de Lanaudière à Joliette</p></div>
-            <div class="col my-auto"><a href="/"><img height={52} width={43} src={Logo} className="d-block mx-auto" alt="Logo" /></a></div>
-            <div class="col my-auto text-end"><a className="h5 text-muted nav-link" href="https://cegep-lanaudiere-joliette.omnivox.ca">Retour à Omnivox</a></div>
+            <div class="col-5 my-auto text-start"><p className="h5 text-muted">©2022 Cégep régional de Lanaudière à Joliette</p></div>
+            <div class="col-2 my-auto"><a href="/"><img height={52} width={43} src={Logo} className="d-block mx-auto" alt="Logo" /></a></div>
+            <div class="col-5 my-auto text-end"><a className="h5 text-muted nav-link" href="https://cegep-lanaudiere-joliette.omnivox.ca">Retour à Omnivox</a></div>
           </div>
       </Card.Footer>
     </Card>
