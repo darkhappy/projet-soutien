66342d220767ff70307a7cc8641abfe0c05e8bb7
SprayZ-prog <lfveillette@outlook.com>
2022-03-07 13:28:31 -0500

    index de Louis

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,6 @@ export default function Header(props) {
           <h1 className="display-2 fw-bold">{props.title}</h1>
           <h2 className="fs-2">{props.subtitle}</h2>
         </div>
-        <div>
-          <img src={LogoImage} alt="Logo du site"/>
-        </div>
       </Container>
     </header>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,9 @@
 import Header from "../components/Header";
 import { Container } from "react-bootstrap";
 import LogoImage from "../img/Shield_Logo.png";
+import Blondinais from "../img/Blondinais.jpg";
+import Black from "../img/black.jpg";
+import gang from "../img/coding_gang.jpg";
 import "../scss/loader.css";
 import "../js/loader.js";
 
@@ -21,6 +24,17 @@ export default function Home() {
       <Container>
       <p>Bienvenue sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
       <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous, nos objectifs, etc.</p>
+      <div className="row">
+        <div className="col-4">
+          <img src={Blondinais} className="w-100 h-100 img-fluid"/>
+        </div>
+        <div className="col-4">
+          <img src={Black} className="w-100 h-100 img-fluid"/>
+        </div>
+        <div className="col-4">
+          <img src={gang} className="w-100 h-100 img-fluid"/>
+        </div>
+      </div>
       </Container>
     </>
   );
