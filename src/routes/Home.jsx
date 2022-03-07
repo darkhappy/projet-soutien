4c552adb01ecabc5243d15c803bd96caa3a1e9cb
darkhappy <github@darkhappy.ca>
2022-03-07 13:31:39 -0500

    Reformatted index

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,29 +12,33 @@ export default function Home() {
     <>
       <div id="overlay">
         <div id="overlay-logo">
-          <img src={LogoImage} alt="Logo du site"/>
+          <img src={LogoImage} alt="Logo du site" />
           <p>Centre d'Aide Informatique</p>
         </div>
       </div>
       <Header
         title="Accueil"
         subtitle="Bienvenue au Centre d'Aide Informatique"
-        logo
       />
       <Container>
-      <p>Bienvenue sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
-      <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous, nos objectifs, etc.</p>
-      <div className="row">
-        <div className="col-4">
-          <img src={Blondinais} className="w-100 h-100 img-fluid"/>
+        <p>Bienvenue sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous
+          pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut
+          rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous
+          pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en
+          expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
+        <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous,
+          nos objectifs, etc.</p>
+        <div className="row">
+          <div className="col-4">
+            <img src={Blondinais} className="w-100 h-100 img-fluid"  alt="Image"/>
+          </div>
+          <div className="col-4">
+            <img src={Black} className="w-100 h-100 img-fluid" alt="Image"/>
+          </div>
+          <div className="col-4">
+            <img src={gang} className="w-100 h-100 img-fluid" alt="Image"/>
+          </div>
         </div>
-        <div className="col-4">
-          <img src={Black} className="w-100 h-100 img-fluid"/>
-        </div>
-        <div className="col-4">
-          <img src={gang} className="w-100 h-100 img-fluid"/>
-        </div>
-      </div>
       </Container>
     </>
   );
