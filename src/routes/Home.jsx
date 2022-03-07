54ffde23e0b06a0be74f0f92992101aa3e3a0112
darkhappy <github@darkhappy.ca>
2022-03-07 14:26:25 -0500

    Reformatted index

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -21,19 +21,24 @@ export default function Home() {
         subtitle="Bienvenue au Centre d'Aide Informatique"
       />
       <Container className="mb-lg-5">
-      <p>Bienvenue sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
-      <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous, nos objectifs, etc.</p>
-      <div className="row">
-        <div className="col-4">
-          <img alt="Etudiant sur son portable" src={Blondinais} className="w-100 h-100 img-fluid"/>
+        <p>Bienvenue sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous
+          pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut
+          rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous
+          pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en
+          expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
+        <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous,
+          nos objectifs, etc.</p>
+        <div className="row">
+          <div className="col-4">
+            <img alt="Etudiant sur son portable" src={Blondinais} className="w-100 h-100 img-fluid" />
+          </div>
+          <div className="col-4">
+            <img alt="Etudiant sur son portable" src={Confus} className="w-100 h-100 img-fluid" />
+          </div>
+          <div className="col-4">
+            <img alt="Etudiant sur son ordinateur" src={gang} className="w-100 h-100 img-fluid" />
+          </div>
         </div>
-        <div className="col-4">
-          <img alt="Etudiant sur son portable" src={Confus} className="w-100 h-100 img-fluid"/>
-        </div>
-        <div className="col-4">
-          <img alt="Etudiant sur son ordinateur" src={gang} className="w-100 h-100 img-fluid"/>
-        </div>
-      </div>
       </Container>
     </>
   );
