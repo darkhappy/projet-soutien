2f52803fc92040cdb5043dab105dc7d666e62d73
darkhappy <github@darkhappy.ca>
2022-03-07 16:05:06 -0500

    Fixed up home a bit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -21,21 +21,21 @@ export default function Home() {
         title="Accueil"
         subtitle="Bienvenue au Centre d'Aide Informatique"
       />
-
-      <Row>
-        <Col>
-          <CurrentCard location="biblio" />
-        </Col>
-        <Col>
-          <CurrentCard location="C-220" />
-        </Col>
-      </Row>
-
-      <Container className="mb-3">
-        <div className="p-3 m-3 border border-2 border-radius-5">
+      <Container className="my-3">
+        <div className="p-3 border border-2 border-radius-5">
           <p><b>Bienvenue</b> sur le site du CAI. Ici vous pourrez connaître nos horaires pour pouvoir nous contacter. Vous pourrez aussi avoir accès à notre documentation pour aider dans certains problèmes informatiques que l’on peut rencontrer dans la vie de tous les jours, ne serait-ce du côté réseau, logiciel et gestion de bureau. Vous pourrez ainsi grâce à ce site vous familiariser avec l’informatique. Sachant que l’informatique est en expansion dans les domaines de travail, il est rendu primordial de s’y connaître. </p>
           <p>Il y a aussi une section « À propos » dans notre menu qui vous permettra dans connaître un peu plus sur nous, nos objectifs, etc.</p>
         </div>
+
+        <Row>
+          <Col>
+            <CurrentCard location="biblio" />
+          </Col>
+          <Col>
+            <CurrentCard location="C-220" />
+          </Col>
+        </Row>
+
         <div className="row mt-3">
         <div className="col-4">
           <img alt="Etudiant sur son portable" src={Blondinais} className="w-100 h-100 img-fluid"/>
