310668345d05e392026727b0f82e65d259d0df8c
SprayZ-prog <78389117+SprayZ-prog@users.noreply.github.com>
2022-03-07 12:02:56 -0500

    Added content to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { Container } from "react-bootstrap";
+import LogoImage from "../img/Shield_Logo.png";
 
 export default function Header(props) {
   return (
     <header className="mb-1 bg-primary">
       <Container className="py-2">
-        <h1 className="display-2 fw-bold">{props.title}</h1>
-        <h2 className="fs-2">{props.subtitle}</h2>
+        <div>
+          <h1 className="display-2 fw-bold">{props.title}</h1>
+          <h2 className="fs-2">{props.subtitle}</h2>
+        </div>
+        <div>
+          <img src={LogoImage} alt="Logo du site"/>
+        </div>
       </Container>
     </header>
   );
