4aed771eba829205094fc545a402c7f378e1d965
eugene-emmanueldubuc <eugene-emmanuel@hotmail.com>
2022-03-15 17:27:19 -0400

    Updating 404 error page

--- src/routes/404/404.jsx
+++ src/routes/404/404.jsx
@@ -1,3 +1,29 @@
+import {Helmet} from "react-helmet";
+import {Header} from "../../components";
+import {Button, Container} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
 export default function page404() {
-  return <h1>404 lol</h1>;
+  const title = "404";
+  const subtitle = "Page non trouvée";
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <Header title={title} subtitle={subtitle} />
+      <Container className="my-3 text-center">
+        <h3 className="my-5">
+          Il semblerait que la page que vous essayez d'atteindre ne soit pas disponible
+        </h3>
+        <Link to="/">
+          <Button className="btn btn-primary" size="lg" type="button">
+            Retour à l'accueil
+          </Button>
+        </Link>
+      </Container>
+    </>
+  );
 }
